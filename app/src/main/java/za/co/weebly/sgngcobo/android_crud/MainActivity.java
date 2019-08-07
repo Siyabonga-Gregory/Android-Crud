7e9be02fe5db0526d0208a8d2c7302d6f174647e
Gregory <huge.fuze@gmail.com>
2019-08-07 13:49:51 +0200

    Clean Up
    Removed activity_create.xml and Create.java

--- app/src/main/java/za/co/weebly/sgngcobo/android_crud/MainActivity.java
+++ app/src/main/java/za/co/weebly/sgngcobo/android_crud/MainActivity.java
@@ -3,6 +3,8 @@ package za.co.weebly.sgngcobo.android_crud;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.weebly.ghostcoder.carservices.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
